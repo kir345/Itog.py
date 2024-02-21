adecb00adbf29b09d7019d4cf32832276b9fb304
kir345 <123649165+kir345@users.noreply.github.com>
2024-02-21 15:49:50 +0300

    Add files via upload

--- itog_log.py
+++ itog_log.py
@@ -13,19 +13,20 @@
 import logging
 from collections import namedtuple
 import os
-import argparse
+import argparse 
 
-FORMAT = '{asctime} - {livelname} - {msg}'
-logging.basicConfig(filename= 'list.log', 
+
+FORMAT = '{asctime} - {msg}'
+logging.basicConfig(filename= 'file_list.log', 
                     filemode= 'w', 
                     format= FORMAT,
                     encoding= 'utf-8',
-                    level= logging.NOTSET, style='{')
+                    level= logging.INFO, style='{')
 
 com_log = logging.getLogger(__name__)
-File = namedtuple('File',['extension', 'file_name', 'parent_dir'])
+File = namedtuple('File', ['name', 'extension', 'is_directory', 'parent_dir'])
 
-def direct_way():
+def direct_way(file):
     objects = []
     pars = argparse.ArgumentParser(description= 'gets the path in the console')
     pars.add_argument('file_list', type= str, default= '.', help= 'enter the path to the directory')
@@ -33,16 +34,31 @@ def direct_way():
 
     for root, files in os.walk(args.file_list):
         for file in files:
-           file_name, extension = os.path.splitext(file)
+           name, is_directory, extension = os.path.splitext(file)
            parent_dir = root.split('/')[-1]
-           file = File(extension, file_name, parent_dir)
+           file = File(name, extension, is_directory, parent_dir)
            objects.append(file)
            com_log.info(file)
 
         return objects
     
 if __name__ == '__main__':
-    print(direct_way())
+    import sys
+
+    if len(sys.argv) != 2:
+        print('Используй: python itog_log.py <directory_path>')
+        sys.exit(1)
+
+    directory_path = sys.argv[1]
+
+    files_list = direct_way(directory_path)
+
+    if files_list:
+        for file_list in files_list:
+            logging.info(f'\n ○ Имя файла: {file_list.name}\n ○ Расширение файла: {file_list.extension}'
+                         f'\n ○ Это директория?: {file_list.is_directory}\n ○ Родительская директория: {file_list.parent_dir}')
+    else:
+        logging.warning('Нет файлов в указанной директории.')
 
     
 
